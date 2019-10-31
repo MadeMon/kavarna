7866b574e16554a860bb80213170cbdf2d4e037a
Mazej <matelf.kubik@gmail.com>
2019-10-31 13:59:51 +0100

    a

--- kavarna/src/main/java/kavarna/Kavarna.java
+++ kavarna/src/main/java/kavarna/Kavarna.java
@@ -10,38 +10,47 @@ import produkt.*;
  */
 public class Kavarna {
 
-    private HashMap<String, Produkt> sklad = new HashMap<String, Produkt>();
+    private HashMap<String, Produkt> skladProdukt = new HashMap<String, Produkt>();
+    private HashMap<String, Integer> skladIngredience = new HashMap<String, Integer>();
     private Pokladna pokladna = new Pokladna(500);
 
-    public String pridejProdukt(String nazev, float cena, String druh) {
-        if (sklad.get(nazev) != null)
+    public String pridejProdukt(String nazev, float cena, String druh, String[] suroviny, int[] mnozstvi) {
+        if (skladProdukt.get(nazev) != null)
             return String.format("%s %s jiz existuje...", druh, nazev);
         Produkt produkt;
         if (druh == "kolac")
-            produkt = new Kolac(nazev, cena);
+            produkt = new Kolac(nazev, cena, suroviny, mnozstvi);
         else if (druh == "kava")
-            produkt = new Kava(nazev, cena);
+            produkt = new Kava(nazev, cena, suroviny, mnozstvi);
         else
             return "Byl spatne zadan druh produktu...";
-        sklad.put(nazev, produkt);
+        skladProdukt.put(nazev, produkt);
         return produkt.toString();
 
     }
 
     public void vyrob(String nazev, int kolik) {
-        Produkt produkt = sklad.get(nazev);
+        Produkt produkt = skladProdukt.get(nazev);
         if (produkt == null)
             System.out.println("Nebyl nalezen produkt");
         produkt.vyrob(kolik);
     }
 
     public String prodej(String nazev, int kolik) {
-        Produkt produkt = sklad.get(nazev);
+        Produkt produkt = skladProdukt.get(nazev);
         if (produkt == null)
             return String.format("Nebyl nalezen produkt %s!", nazev);
+/**
+ * TODO aby se pri prodeji kava vyrobila a odecetly se ingredience ze skladu
+ * TODO svytvorit classy pro sklad ingredienci a ingredience v Produktu
+ */
+        if (produkt.getClass().getName() == "Kava") {
+            for(String surovina:produkt.getIngredience())
+            skladIngredience.put(surovina,skladIngredience.get(surovina)-produkt.getIngredience())
+        }
         String vysledek = produkt.prodej(kolik);
         if (vysledek == "nedostatek") {
-            return String.format("Nedostatek %s na sklade, zbyva %d...", nazev, produkt.getMnozstvi());
+            return String.format("Nedostatek %s na skladProdukte, zbyva %d...", nazev, produkt.getPocet());
         } else if (produkt.prodej(kolik) == "") {
             pokladna.pridej(produkt.getCena() * kolik);
             return String.format("Tady mate %d %s", kolik, nazev);
@@ -52,14 +61,28 @@ public class Kavarna {
 
     public String vypisNabidku() {
         String output = "\n\nNAZEV\tCENA\tMNOZSTVI";
-        for (Produkt produkt : sklad.values()) {
-            output += String.format("\n%s\t%f\t%d", produkt.getNazev(), produkt.getCena(), produkt.getMnozstvi());
+        for (Produkt produkt : skladProdukt.values()) {
+            output += String.format("\n%s\t%f\t%d", produkt.getNazev(), produkt.getCena(), produkt.getPocet());
         }
         return output;
     }
 
     public String zustatekPokladny() {
         return this.pokladna.toString();
+    }
+
+    // pridavani nove ingredience i doplnovani ingredience
+    public void pridejIngredienci(String nazev, int kolik) {
+        skladIngredience.put(nazev, skladIngredience.get(nazev) + kolik);
+    }
+
+    public void pridejIngredienci(String nazev) {
+        skladIngredience.put(nazev, skladIngredience.get(nazev));
+    }
+
+    public void pridejIngredienci(String[] nazvy, int[] kolik) {
+        for (int i = 0; i < nazvy.length; i++)
+            skladIngredience.put(nazvy[i], skladIngredience.get(nazvy[i]) + kolik[i]);
     }
 }
 
--- kavarna/src/main/java/kavarna/Main.java
+++ kavarna/src/main/java/kavarna/Main.java
@@ -1,31 +1,30 @@
 package kavarna;
 
+import scala.Array;
+
 class Main {
     public static void main(String[] args) {
         Kavarna k = new Kavarna();
-        k.pridejProdukt("espresso", 25.2f, "kava");
-        k.pridejProdukt("Makovak", 40.2f, "kolac");
-        k.pridejProdukt("Mrkvak", 35.6f, "kolac");
-        k.pridejProdukt("Latte", 40f, "kava");
+        String[] suroviny = { "mleko", "mouka", "cukr", "maslo", "olej", "espresso" };
+        int[] mnozstvi = { 5000, 6000, 3000, 4000, 1000, 1000 };
+        k.pridejIngredienci(suroviny, mnozstvi);
+        String[] surovinyEspresso = { "espresso" };
+        int[] mnozstviEspresso = { 30 };
+        k.pridejProdukt("espresso", 25.2f, "kava", surovinyEspresso, mnozstviEspresso);
+        // k.pridejProdukt("Makovak", 40.2f, "kolac");
+        // k.pridejProdukt("Mrkvak", 35.6f, "kolac");
+        // k.pridejProdukt("Latte", 40f, "kava");
+
         k.vyrob("espresso", 4);
-        k.vyrob("Makovak", 3);
-        k.vyrob("Mrkvak", 6);
-        k.vyrob("Latte", 9);
+        // k.vyrob("Makovak", 3);
+        // k.vyrob("Mrkvak", 6);
+        // k.vyrob("Latte", 9);
         System.out.println(k.vypisNabidku());
         System.out.println(k.prodej("Makovak", 5));
         System.out.println(k.prodej("askldfjl", 5));
         System.out.println(k.prodej("Latte", 2));
         System.out.println(k.vypisNabidku());
         System.out.println(k.zustatekPokladny());
-        // Produkt espresso = new Kava("espresso", 50);
-        // Produkt makovak = new Kolac("Makovak",30);
-        // System.out.println(makovak);
 
-        // sklad.pridej(espresso, 3);
-        // sklad.pridej("espresso",4);
-        // sklad.pridej(makovak,10);
-        // System.out.print(sklad);
-
-        // prodej("Makovak",2);
     }
 }
